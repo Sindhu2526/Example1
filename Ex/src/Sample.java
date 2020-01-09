8de0df7e6a918febdcd14fde4c298ff300b5936a
Sindhu2526 <59596571+Sindhu2526@users.noreply.github.com>
2020-01-09 15:31:17 +0530

    Update Sample.java

--- Ex/src/Sample.java
+++ Ex/src/Sample.java
@@ -1,6 +1,7 @@
 
 public class Sample {
  public static void main(String[] args) {
-	System.out.println("sindhuuuuuuu111111111");
+	System.out.println("sindhuuuuuuu11111111");
+	 public String(){}
 }
 }
